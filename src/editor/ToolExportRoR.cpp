9a6bc8cfabd49488c026e2a0eb7a948fb812b363
cryham <cryham@gmail.com>
2024-02-11 14:12:47 +0100

    cd ror export copy lay tex, save more files

--- src/editor/ToolExportRoR.cpp
+++ src/editor/ToolExportRoR.cpp
@@ -3,28 +3,67 @@
 #include "BaseApp.h"
 #include "settings.h"
 #include "paths.h"
+
 #include "CApp.h"
 #include "CGui.h"
 #include "CScene.h"
-#include "Road.h"
-#include "paths.h"
+#include "CData.h"
+#include "TracksXml.h"
+#include "Axes.h"
+// #include "Road.h"
+
 #include <string>
+#include <filesystem>
+namespace fs = std::filesystem;
 using namespace Ogre;
 using namespace std;
 
 
+//  gui events
+void CGui::editRoRPath(Ed ed)
+{
+	pSet->pathExportRoR = ed->getCaption();
+}
+void CGui::btnExport(WP)
+{
+	app->ToolExportRoR();
+}
+
 //  Export current track for RigsOfRods
+//------------------------------------------------------------------------------------------------------------------------
 void App::ToolExportRoR()
 {
-	//  gui status
+	//  Gui status
 	gui->Status("RoR Export..", 1,0.5,1);
-	gui->edWarn->setCaption("");
-	gui->Warn(CGui::INFO, "Export to RoR started..");
+	gui->edExport->setCaption("");
+	gui->Exp(CGui::INFO, "Export to RoR started..");
 
 
-	//  heightmap convert to .raw
+	//  dir  export to RoR content path
+	string& dirRoR = pSet->pathExportRoR;
+	if (dirRoR.empty())
+	{	gui->Exp(CGui::ERR, "Export path empty. Need to set export RoR path first.");
+		return;
+	}
+	//  end with /
+	if (!StringUtil::endsWith(dirRoR, "\\") &&
+		!StringUtil::endsWith(dirRoR, "/"))
+		dirRoR += "/";
+
+	//  subdir  track name
+	const string trk = "^" + pSet->gui.track;  // ^ for on top
+	const string dirTrk = dirRoR + trk;
+	if (!PATHS::CreateDir(dirTrk))
+	{	gui->Exp(CGui::ERR, "Can't create track dir: "+dirTrk);
+		return;
+	}
+	const string path = dirTrk + "/";  // main
+	const auto& sc = scn->sc;
+
+
+	//  ⛰️ Heightmap convert to .raw
 	//------------------------------------------------------------
-	auto& td = scn->sc->tds[0];  // 1st ter only
+	const auto& td = sc->tds[0];  // 1st ter only
 
 	const int size  = td.iVertsX;  // org  e.g. 1024
 	const int size1 = size + 1;  // e.g. 1025
@@ -34,7 +73,7 @@ void App::ToolExportRoR()
 	memset(hmap, 0, hsize * 2);
 
 	//  find h min, max
-	float hmin = 100000.f, hmax = -100000.f;
+	float hmin = 100000.f, hmax = -hmin;
 	for (int y=0; y < size; ++y)
 	{
 		int a = y * size;
@@ -45,52 +84,134 @@ void App::ToolExportRoR()
 			if (h < hmin)  hmin = h;
 	}	}
 	const float hsc = 1.f / (hmax - hmin);
+	
+	//  world size
 	const float Ysize = hmax - hmin;
-	// const float hsc = max( fabs(hmax), fabs(hmin) );
-	// const float hmul = 12767.f / hsc;
+	const float XZsize = size *  td.fTriangleSize;
 
-	gui->Warn(CGui::TXT, "H min    " + toStr(hmin));
-	gui->Warn(CGui::TXT, "H max    " + toStr(hmax));
-	gui->Warn(CGui::TXT, "H scale   " + toStr(Ysize));
+	gui->Exp(CGui::TXT, "Hmap size   " + toStr(size));
+	gui->Exp(CGui::TXT, "Height min  " + toStr(hmin));
+	gui->Exp(CGui::TXT, "Height max  " + toStr(hmax));
+	gui->Exp(CGui::TXT, "World XZ size  " + toStr(XZsize));
+	gui->Exp(CGui::TXT, "World Y size   " + toStr(Ysize));
 
 	//  normalize  to 2B raw:  0..65535
 	for (int y=0; y < size; ++y)
 	{
 		int a = y * size;
-		// int a1 = y * size1;
-		// int a1 = (size-1 - y) * size1 + size-1;  // flip x,y
-		
-		// for (int x=0; x < size; ++x,++a,--a1)
-		// 	hmap[a1] = (td.hfHeight[a] - hmin) * hsc * 32767.f + 32767.f;
-
 		for (int x=0; x < size; ++x,++a)
-			hmap[x*size1 + y] = (td.hfHeight[a] - hmin) * hsc * 32767.f + 32767.f;
+			hmap[x*size1 + y] =  // flip x-y
+				(td.hfHeight[a] - hmin) * hsc * 32767.f + 32767.f;
 	}
 
+	//  todo works?
 	//  add extra +1 col  |
-	for (int y=0; y < size; ++y)  hmap[y*size + size-1] = hmap[y*size + size-2];
+	for (int y=0; y < size; ++y)
+		hmap[y * size + size-1] = hmap[y * size + size-2];
+	
 	//  add extra +1 row __
-	const int a = (size-1)*size, a1 = (size-2)*size;
-	for (int x=0; x < size; ++x)  hmap[x + a] = hmap[x + a1];
+	const int a = (size-1) * size, a1 = (size-2) * size;
+	for (int x=0; x < size; ++x)
+		hmap[x + a] = hmap[x + a1];
 
 
-	//  save hmap .raw 16bit
+	//  save Hmap  .raw  16bit 2B
 	//------------------------------------------------------------
-	string path = "/home/ch/_p/rigs-of-rods-dev/content/``0";  // adj..
-	string hmapFile = path + "/heightmap.raw";
+	const string hmapFile = path + "heightmap.raw";
+	
 	ofstream of;
 	of.open(hmapFile.c_str(), std::ios_base::binary);
 	of.write((const char*)&hmap[0], hsize * 2);
 	of.close();
 	delete[] hmap;  hmap = 0;
 
-	
-	//  world size
-	const float XZsize = size *  td.fTriangleSize;
 
-	//  save  .otc  Terrain setup
+	//  🏔️ Blendmap
 	//------------------------------------------------------------
-	string otcFile = path + "/0.otc";
+	gui->Exp(CGui::TXT, "Terrain layers  " + toStr(td.layers.size()));
+
+	//  copy layer textures
+
+	int layers = std::min(4, (int)td.layers.size());
+	for (int i=0; i < layers; ++i)
+	{
+		const TerLayer& l = td.layersAll[td.layers[i]];
+		// di.layerList[i].worldSize = l.tiling;
+
+		//  combined rgb,a from 2 tex
+		String pathTerTex = PATHS::Data() + "/terrain/";
+		String diff = l.texFile;  // ends with _d
+		String norm = l.texNorm;  // _n
+		gui->Exp(CGui::TXT, "layer " + toStr(i+1) + " diff, norm:  " + diff + "  " + norm);
+		
+		string from, to;
+		try
+		{
+			from = pathTerTex + diff;  to = path + diff;
+			if (!fs::exists(to.c_str()))
+				fs::copy_file(from.c_str(), to.c_str());
+
+			from = pathTerTex + norm;  to = path + norm;
+			if (!fs::exists(to.c_str()))
+				fs::copy_file(from.c_str(), to.c_str());
+		}
+		catch (const fs::filesystem_error & ex)
+		{
+			String s = "Error: Copying file " + from + " to " + to + " failed ! \n" + ex.what();
+			gui->Exp(CGui::WARN, s);
+		}
+
+		// todo combine RGB+A ..  diff+spec .. ?
+		// todo convert blending to lerp(lerp( .. ??
+	}
+
+	
+	//  📄🏔️ Terrain layers setup  save  page.otc
+	//------------------------------------------------------------------------------------------------------------------------
+	string opgFile = path + "0-page-0-0.otc";
+	ofstream lay;
+	lay.open(opgFile.c_str(), std::ios_base::out);
+
+	lay << "heightmap.raw\n";
+	// opg << "2\n";  // todo
+	lay << layers << "\n";
+	lay << "; worldSize, diffusespecular, normalheight, blendmap, blendmapmode, alpha\n";
+
+	// int layers = std::min(4, (int)td.layers.size());  // todo
+	layers = 2;  // 0 ter, 1 road  only ...
+	for (int i=0; i < layers; ++i)
+	{
+		const TerLayer& l = td.layersAll[td.layers[i]];
+		// di.layerList[i].worldSize = l.tiling;
+
+		//  combined rgb,a from 2 tex
+		String pathTerTex = PATHS::Data() + "/terrain/";
+		String diff = l.texFile;  // ends with _d
+		String norm = l.texNorm;  // _n
+
+		if (i==0)
+			lay << l.tiling << " , " << diff+", "+norm+"\n";
+		else
+			lay << l.tiling << " , " << diff+", "+norm+", roadDensity.png, R, 0.99\n";
+
+		//  todo all layers, blendmap R,G,B,A ..
+		// "5    , desert_sand_d.jpg, desert_sand_n.jpg, roadDensity.png, R, 0.99\n";
+	}
+	lay.close();
+
+	int r = 0;
+	Image2 imgRoad;  // fixme outside road
+	try
+	{	imgRoad.load(String("roadDensity.png"),"General");  }
+	catch(...)
+	{	}
+	r = imgRoad.getWidth();
+	gui->Exp(CGui::WARN, "road Dens: "+fToStr(r));
+
+
+	//  📄⛰️ Terrain hmap, setup  save  .otc
+	//------------------------------------------------------------
+	string otcFile = path + "0.otc";
 	ofstream otc;
 	otc.open(otcFile.c_str(), std::ios_base::out);
 
@@ -113,7 +234,7 @@ void App::ToolExportRoR()
 	otc << "WorldSizeY=" << Ysize * 2.f << "\n";
 	otc << "\n";
 	otc << "# Sets the default size of blend maps for a new terrain. This is the resolution of each blending layer for a new terrain. default: 1024\n";
-	otc << "LayerBlendMapSize=2048\n";
+	otc << "LayerBlendMapSize=2048\n";  // todo 1024 ?
 	otc << "\n";
 	otc << "# disableCaching=1 will always enforce regeneration of the terrain, useful if you want to change the terrain config (.otc) and test it. Does not cache the objects on it.\n";
 	otc << "disableCaching=1\n";
@@ -160,15 +281,89 @@ void App::ToolExportRoR()
 	otc << "CastsDynamicShadows=0\n";
 	otc << "\n";
 	otc << "# Set the maximum screen pixel error that should  be allowed when rendering, default:\n";
-	otc << "MaxPixelError=3\n";
+	otc << "MaxPixelError=3\n";  // todo >
 	otc << "\n";
-	otc << "# dump the blend maps to files named blendmap_layer_X.png\n";
-	otc << "DebugBlendMaps=0\n";
-	otc << "NormalMappingEnabled=1\n";
-	otc << "\n";
+	// otc << "# dump the blend maps to files named blendmap_layer_X.png\n";
+	// otc << "DebugBlendMaps=0\n";
+	// otc << "NormalMappingEnabled=1\n";
 
 	otc.close();
 
 
-	gui->Warn(CGui::INFO, "Export to RoR end.");
+	//  get authors from tracks.ini
+	string authors = "CryHam";
+	int id = scn->data->tracks->trkmap[pSet->gui.track];
+	if (id > 0)
+	{	const TrackInfo& ti = scn->data->tracks->trks[id-1];
+
+		authors = ti.author=="CH" ? "CryHam" : ti.author;
+	}
+
+
+	//  🏞️⛅ Track/map setup  save  .terrn2
+	//------------------------------------------------------------------------------------------------------------------------
+	string terrn2File = path + "0.terrn2";
+	ofstream trn;
+	trn.open(terrn2File.c_str(), std::ios_base::out);
+
+	trn << "[General]\n";
+	trn << "Name = "+trk+"\n";
+	trn << "GeometryConfig = 0.otc\n";
+	trn << "Water=0\n";  // todo  find big water h-
+	trn << "WaterLine=1\n";
+
+	trn << "AmbientColor = 0.99, 0.98, 0.97\n";  // todo ? sc->lAmb
+	//trn << "StartPosition = 0.0, 110.0, 0.0\n";  // todo  start
+	Vector3 st = Axes::toOgre(sc->startPos[0]);
+	trn << "StartPosition = " << fToStr(st.x)+", "+fToStr(st.y)+", "+fToStr(st.z)+"\n";
+
+	trn << "#CaelumConfigFile = \n";  // todo  caelum.os
+	trn << "SandStormCubeMap = tracks/skyboxcol\n";  // sky meh-
+
+	trn << "Gravity = " << -sc->gravity << "\n";
+	trn << "CategoryID = 129\n";
+	trn << "Version = 1\n";
+	trn << "GUID = 11112222-e652-4d36-a32b-b9499db7898e\n";  // todo, random hash from trk name?
+	trn << "\n";
+	// trn << "//If your map has a groundmodel, define the landuse file in this format:\n";
+	trn << "#TractionMap = landuse.cfg\n";  // todo  surfaces.cfg
+	trn << "\n";
+	trn << "[Authors]\n";
+	trn << "terrain = " + authors + "\n";
+	trn << "conversion = Exported from Stunt Rally 3 Track Editor\n";
+	trn << " \n";
+	trn << "#[Objects]\n";  // todo  objs
+	trn << "#0.tobj=\n";
+	trn << "\n";
+	trn << "#[Scripts]\n";  // todo  road, checks
+	// trn << "//If your map has a race script define it here in this format:\n";
+	// trn << "//All 0.3x races use .terrn.as as the extension, include the .terrn also:\n";
+	trn << "#0.terrn.as=\n";
+
+	trn.close();
+
+
+	//  📄📦 Objects  save  .tobj
+	//------------------------------------------------------------
+	string objFile = path + "0.tobj";
+	ofstream obj;
+	obj.open(objFile.c_str(), std::ios_base::out);
+
+	for (const auto& o : sc->objects)
+	{
+		Vector3 p = Axes::toOgre(o.pos);
+		auto q = Axes::toOgreW(o.rot);
+		// obj << "257.641, 1.19744, 225.319, -0, 0, 0, 8c07UID-baja_rock_01\n";
+		obj << fToStr(p.x)+", "+fToStr(p.y)+", "+fToStr(p.z);
+		// todo q.toEulerAngles ?
+		obj << "-0, 0, 0, ";
+		obj << o.name +"\n";
+	}
+	//  vegetation?..
+	scn->vegetNodes;
+
+	obj.close();
+
+
+	gui->Exp(CGui::INFO, "Export to RoR end.");
 }
