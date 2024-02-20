6d13d5541bb232fdf3117129d9bf6e1348e434ca
cryham <cryham@gmail.com>
2024-02-20 20:09:00 +0100

    ed ror fix road yaw, add checks, races

--- src/editor/ExportRoR_Road.cpp
+++ src/editor/ExportRoR_Road.cpp
@@ -38,136 +38,142 @@ void ExportRoR::ExportRoad()
 {
 	Ogre::Timer ti;
 
-	std::vector<string> chks;
+	std::vector<string> chks;  int chk1st = 0;
 	const bool roadtxt = !scn->roads.empty();
 	const bool road = roadtxt && scn->roads[0]->getNumPoints() > 2;
 	const float fLenDim = cfg->roadStepDist;  // points density
 
-	hasRoad = 0;
-	if (road)
-	{	const auto& rd = scn->roads[0];
+	hasRoad = 0;  hasRoadChks = 0;
+	if (!road)
+	{	gui->Exp(CGui::WARN, "No Road\n");
+		return;
+	}
 
-		//  if all points on ter, skip road, is on blendmap
-		bool roadOnTer = 1;
-		for (int i=0; i < rd->getNumPoints(); ++i)
-			if (!rd->getPoint(i).onTer)
-				roadOnTer = 0;
+	const auto& rd = scn->roads[0];
 
-		//  has some bridges  ------------------------
-		if (!roadOnTer)
-		{	hasRoad = 1;
-			string roadFile = path + name + "-road.tobj";
-			ofstream trd;
-			trd.open(roadFile.c_str(), std::ios_base::out);
+	//  if all points on ter, skip road, is on blendmap
+	bool roadOnTer = 1;
+	for (int i=0; i < rd->getNumPoints(); ++i)
+		if (!rd->getPoint(i).onTer)
+			roadOnTer = 0;
 
-			trd << "//  position x,y,z   rotation rx,ry,rz,   width,   border width, border height,  type\n";
-			bool begin = 0;  int iroads = 0;
+	//  has some bridges  ------------------------
+	if (roadOnTer)
+		gui->Exp(CGui::NOTE, "Whole road on terrain");
+	else
+	{	hasRoad = 1;
+		string roadFile = path + name + "-road.tobj";
+		ofstream trd;
+		trd.open(roadFile.c_str(), std::ios_base::out);
 
-			for (int i=0; i < rd->getNumPoints() + 1; ++i)  // loop it
-			{
-				//  i0,1
-				const int i0 = rd->getAdd(i,0), i1 = rd->getNext(i);
-				const auto& p = rd->getPoint(i0), p1 = rd->getPoint(i1);
-				bool bridge = !p.onTer || !p1.onTer;
-				if (p.idMtr < 0)  bridge = 0;  // hidden
+		trd << "//  position x,y,z   rotation rx,ry,rz,   width,   border width, border height,  type\n";
+		bool begin = 0;  int iroads = 0;
 
-				if ( (i==0 || !begin) && bridge)
-				{	begin = 1;  ++iroads;
-					trd << "begin_procedural_roads\n";
-					// gui->Exp(CGui::TXT, "Road begin");
-				}
-
-				// gui->Exp(CGui::TXT, "Road i0: "+toStr(i0)+"  ter: "+toStr(p.onTer?1:0) +"  l "+fToStr(len) );
-
-				//  add points on bridge  ----------
-				if (begin)
-				{
-					//  length steps  |
-					// const int il = 2;  // const LQ
-					Real len = rd->GetSegLen(i0);
-					const int il = 1 + (len / fLenDim);  // var, by dist
-					// todo more with bigger angle changes?
-					const int ila = il + (!bridge ? 1 : 0);  // bridge will end, use all length points
-					// gui->Exp(CGui::TXT, "Road il: "+toStr(il) );
-					
-					for (int l = 0; l < ila; ++l)
-					{	//  pos
-						float fl = l;
-						Vector3 vP = rd->interpolate(i0, fl / il);
-						Vector3 vP1 = rd->interpolate(i0, (fl+1.f) / il);
-						Vector3 dir = vP1 - vP;  // along length
-
-						const float width = rd->interpWidth(i0, fl / il);
-
-						// if (p.onTer)  // ?
-						// 	vP.y = scn->getTerH(vP.x, vP.z);
-						vP.y += cfg->roadHadd;  // lower for entry  // -0.9f + rd->g_Height;
-
-						//  rot y
-						float yaw = TerUtil::GetAngle(dir.x, dir.z) *180.f/PI_d - 45.f - 15.f;  // par ? 45
-
-						//  write  ------
-						//  pos
-						// trd << half - vP.z << ", " << vP.y - hmin << ", " << vP.x + half << ",   ";
-						trd << strPos(vP) << "  ";
-						//  rot
-						trd << "0, " << yaw << ", 0,  ";  // todo  p.aRoll
-						trd << width << ",   ";
-						
-						//  bridge
-						//if (p.onTer)  trd << "0.5,  0.2,  flat\n";  else
-						//  trd << "0.6,  1.0,  bridge\n";
-						// bool low = p.onTer;
-						bool low = p.onTer && (l == 0 || l == ila-1);
-						trd << cfg->wallX << ",  " << (low ? 0.f : cfg->wallY) << 
-							(cfg->roadCols ? ",  bridge\n" : ",  bridge_no_pillars\n");
-				}	}
-
-	
-				if (begin && !bridge)
-				{	begin = 0;
-					trd << "end_procedural_roads\n";
-					// gui->Exp(CGui::TXT, "Road end");
-				}
-			}
-			if (begin)
-				trd << "end_procedural_roads\n";
-
-			/*	0,0,0,         0,0,0,    10.0,  0,            0,             flat
-				0,0,0,         0,0,0,    10.0,  0.25,         1.0,           both
-				flat - none
-				left, right, both - borders
-				bridge, bridge_no_pillars
-			*/
-			trd.close();
-			gui->Exp(CGui::NOTE, "Roads: "+toStr(iroads));
-		}
-
-		//  get checkpoints
 		for (int i=0; i < rd->getNumPoints() + 1; ++i)  // loop it
 		{
-			const auto& p = rd->getPoint(i);
-			if (p.chkR > 0.1f)
+			//  i0,1
+			const int i0 = rd->getAdd(i,0), i1 = rd->getNext(i);
+			const auto& p = rd->getPoint(i0), p1 = rd->getPoint(i1);
+			bool bridge = !p.onTer || !p1.onTer;
+			if (p.idMtr < 0)  bridge = 0;  // hidden
+
+			if ( (i==0 || !begin) && bridge)
+			{	begin = 1;  ++iroads;
+				trd << "begin_procedural_roads\n";
+				// gui->Exp(CGui::TXT, "Road begin");
+			}
+
+			// gui->Exp(CGui::TXT, "Road i0: "+toStr(i0)+"  ter: "+toStr(p.onTer?1:0) +"  l "+fToStr(len) );
+
+			//  add points on bridge  ----------
+			if (begin)
 			{
-				// Vector3 vP = p.pos;
-				Vector3 vP = rd->interpolate(i, 0.f);
-				Vector3 vP1 = rd->interpolate(i, 0.2f);  // par-
-				Vector3 dir = vP1 - vP;  // along length
-				float yaw = TerUtil::GetAngle(dir.x, dir.z) *180.f/PI_d - 45.f - 15.f;  // par ? 45
-				// pos 
-				string s = strPos(vP) + "  ";
-				// rot
-				s += "0.0,  " + fToStr(yaw) + ",  0.0";
-				chks.push_back(s);
-		}	}
+				//  length steps  |
+				// const int il = 2;  // const LQ
+				Real len = rd->GetSegLen(i0);
+				const int il = 1 + (len / fLenDim);  // var, by dist
+				// todo more with bigger angle changes?
+				const int ila = il + (!bridge ? 1 : 0);  // bridge will end, use all length points
+				// gui->Exp(CGui::TXT, "Road il: "+toStr(il) );
+				
+				for (int l = 0; l < ila; ++l)
+				{	//  pos
+					float fl = l;
+					Vector3 vP = rd->interpolate(i0, fl / il);
+					Vector3 vP1 = rd->interpolate(i0, (fl+1.f) / il);
+					Vector3 dir = vP1 - vP;  // along length
+
+					const float width = rd->interpWidth(i0, fl / il);
+
+					// if (p.onTer)  // ?
+					// 	vP.y = scn->getTerH(vP.x, vP.z);
+					vP.y += cfg->roadHadd;  // lower for entry  // -0.9f + rd->g_Height;
+
+					//  rot y
+					float yaw = TerUtil::GetAngle(dir.x, dir.z) *180.f/PI_d - 90.f;  // ok
+
+					//  write  ------
+					//  pos
+					// trd << half - vP.z << ", " << vP.y - hmin << ", " << vP.x + half << ",   ";
+					trd << strPos(vP) << "  ";
+					//  rot
+					trd << "0, " << yaw << ", 0,  ";  // todo  p.aRoll
+					trd << width << ",   ";
+					
+					//  bridge
+					//if (p.onTer)  trd << "0.5,  0.2,  flat\n";  else
+					//  trd << "0.6,  1.0,  bridge\n";
+					// bool low = p.onTer;
+					bool low = p.onTer && (l == 0 || l == ila-1);
+					trd << cfg->wallX << ",  " << (low ? 0.f : cfg->wallY) << 
+						(cfg->roadCols ? ",  bridge\n" : ",  bridge_no_pillars\n");
+					// 	flat - none   left, right, both - borders   bridge, bridge_no_pillars
+			}	}
+
+
+			if (begin && !bridge)
+			{	begin = 0;
+				trd << "end_procedural_roads\n";
+				// gui->Exp(CGui::TXT, "Road end");
+			}
+		}
+		if (begin)
+			trd << "end_procedural_roads\n";
+
+		trd.close();
+		gui->Exp(CGui::NOTE, "Roads: "+toStr(iroads));
 	}
+
+
+	//  get checkpoints
+	//------------------------------------------------------------
+	for (int i=0; i < rd->getNumPoints() + 1; ++i)  // loop it
+	{
+		const auto& p = rd->getPoint(i);
+		if (p.chkR > 0.1f)
+		{
+			//Vector3 vP = p.pos;
+			Vector3 vP = rd->interpolate(i, 0.f);
+			Vector3 vP1 = rd->interpolate(i, 0.2f);  // par-
+			Vector3 dir = vP1 - vP;  // along length
+			float yaw = TerUtil::GetAngle(dir.x, dir.z) *180.f/PI_d /*- 90.f*/;  // ok
+			//  pos 
+			string s = strPos(vP) + "  ";
+			//  rot
+			s += "0.0,  " + fToStr(yaw) + ",  0.0";
+			
+			if (p.chk1st)  chk1st = chks.size();
+			chks.push_back(s);
+	}	}
+
 
 	//  Road script  checks
 	//------------------------------------------------------------
 	const int nchk = chks.size();
-	gui->Exp(CGui::NOTE, "Road checks: " + toStr(nchk));
+	gui->Exp(CGui::NOTE, "Road checks: " + toStr(nchk)+"  first: "+toStr(chk1st));
 	if (nchk > 1)
 	{
+		hasRoadChks = 1;
 		string asFile = path + name + ".as";
 		ofstream as;
 		as.open(asFile.c_str(), std::ios_base::out);
@@ -179,13 +185,13 @@ void ExportRoR::ExportRoad()
 		as << "\n";
 		as << "void createRaces()\n";
 		as << "{\n";
-		as << "	//races.showCheckPointInfoWhenNotInRace = true;\n";
+		as << "    races.showCheckPointInfoWhenNotInRace = true;\n";
 		as << "\n";
-		as << "	races.setCallback(\"RaceFinish\", on_raceFinish);\n";
-		as << "	\n";
-		as << "	double[][] race_autocross = \n";
-		as << "		{\n";
-	#if 1
+		as << "    races.setCallback(\"RaceFinish\", on_raceFinish);\n";
+		as << "    \n";
+		as << "    double[][] race_sr = \n";
+		as << "    {\n";
+	#if 0
 		int i = 0;
 		for (auto ch : chks)
 		{
@@ -193,24 +199,35 @@ void ExportRoR::ExportRoad()
 			as << "			{ " << ch << (last ? " }\n" : " },\n");
 			++i;
 		}
+	#else  // right order
+		int i = chk1st, n = 0, nc = nchk-1;
+		for (n=0; n < nchk; ++n)
+		{
+			bool last = n == nc;  // no ,
+			as << "			{ " << chks[i] << (last ? " }\n" : " },\n");
+			i += rd->iDir;
+			if (i < 0)  i = nc;
+			if (i > nc)  i = 0;
+		}
 	#endif
 		as << "		};\n";
-		as << "	int raceID = races.addRace(\"Autocross\", race_autocross, races.LAPS_Unlimited, \"31-checkpoint\", \"31-checkpoint\", \"31-checkpoint\", \"CryHam-1.0\");\n";
-		as << "	\n";
-		as << "	races.setVersion(raceID, \"CryHam-1.0\");\n";
-		as << "	races.finalize(raceID);\n";
+		as << "	int raceID = races.addRace(\"SR\", race_sr, " <<
+			"races.LAPS_Unlimited, \"sr-checkpoint\");\n";
+			// "races.LAPS_Unlimited, \"sr-checkpoint\", \"sr-start\");\n";
+			//checkpoint object     startline object        finishline object
+			// "\"sr-checkpoint\", \"sr-checkpoint\", \"sr-checkpoint\", \"CryHam-1.0\");\n";
+		// as << "	races.setVersion(raceID, \"CryHam-1.0\");\n";
 		as << "}\n";
 		as << "\n";
 		as << "void main()\n";
 		as << "{\n";
-		as << "	createRaces();\n";
-		as << "	//races.submitScore(false);\n";
+		as << "    createRaces();\n";
+		as << "    //races.submitScore(false);\n";  // ?
 		as << "}\n";
 		as << "\n";
 		as << "void eventCallback(int eventnum, int value)\n";
 		as << "{\n";
-		as << "	// required for the races!\n";
-		as << "	races.eventCallback(eventnum, value);\n";
+		as << "    races.eventCallback(eventnum, value);\n";
 		as << "}\n";
 		as << "\n";
 		as << "//  finishing a race\n";
@@ -222,17 +239,21 @@ void ExportRoR::ExportRoad()
 		as << "    float lapTime = game.getTime() - races.lapStartTime;\n";
 		as << "    float raceTime = game.getTime() - races.raceStartTime;\n";
 		as << "    \n";
-		as << "    // Race specific stuff\n";
-		as << "    if(raceID == races.getRaceIDbyName(\"Autocross\"))\n";
+    	//as << "    game.flashMessage(\"YES\", 3, -1);\n";  // nothing, meh
+		// as << "    races.message(\"FINISH\", \"lightning.png\");\n";  // nothing
+		//as << "    game.message(\"----------------------\", \"chart_bar.png\", 30000, true);\n";
+		as << "    game.message(\"Lap time ++: \" + lapTime , \"chart_bar.png\", 30000, true);\n";
+		as << "    \n";
+		as << "    // Race specific\n";
+		as << "    if (raceID == races.getRaceIDbyName(\"SR\"))\n";
 		as << "    {\n";
 		as << "        // Calculate the average speeds (0.25 mile = 0.40... kilometers)\n";
-		as << "        float averageSpeed_mph = 0.25 / lapTime * 3600;\n";  // todo road length * 0.6
-		as << "        float averageSpeed_kph = 0.402336 / lapTime * 3600;\n";
+		as << "        float avg_mph = 0.25 / lapTime * 3600;\n";  // todo road length * 0.6
+		as << "        float avg_kph = 0.402336 / lapTime * 3600;\n";
 		as << "        \n";
-		as << "        game.message(\"Average speed: \" + averageSpeed_mph + \" mph (\" + averageSpeed_kph + \" km/h).\", \"chart_bar.png\", 30000, true);\n";
+		as << "        game.message(\"Average speed: \" + avg_mph + \" mph (\" + avg_kph + \" km/h).\", \"chart_bar.png\", 30000, true);\n";
 		as << "    }\n";
-		as << "}  \n";
-		as << "\n";
+		as << "}\n";
 
 		as.close();
 	}
